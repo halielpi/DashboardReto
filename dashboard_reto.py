4b7a496bd9b8915b0b398676f23cae7456b506bc
Haliel Pichardo <haliel@live.com>
2023-04-27 19:05:18 -0600

    Lista de integrantes

--- dashboard_reto.py
+++ dashboard_reto.py
@@ -11,7 +11,14 @@ app = dash.Dash()
 
 app.layout = html.Div([
     html.H1('Dashboard para CNSF', style={'textAlign':'center','color':'#1a2e9c', 'background-color': 'lightblue'}),
-    
+    html.H3('Dashboard hecho por:'),
+    html.Ul([
+        html.Li('Alejandra Núñez Galindo (A01654136)'),
+        html.Li('Diego Armando Cortés Mendoza (A01653915)'),
+        html.Li('Haliel Pichardo Jaime (A01654497)'),
+        html.Li('Jorge Jair Licea Ávalos (A01654956)'),
+        html.Li('Maximiliano Barajas Chávez (A01654403)'),
+        ]),
     dcc.Graph(
         id='graph_1',
         figure=bar_emision(),
