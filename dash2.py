03d9054e0b868d5b0a9da6049e689429a60433b9
Haliel Pichardo <haliel@live.com>
2023-05-18 18:04:30 -0600

    add bottons

--- dash2.py
+++ dash2.py
@@ -3,6 +3,7 @@ from dash import dcc, html, Dash
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 import pandas as pd
+from dash import Input, Output
 
 from graph import *
 
@@ -26,21 +27,22 @@ app.layout =dbc.Container([
                         [
                             html.H2('Perfil de los asegurados'),
                             html.Hr(),
-                            dcc.Graph(
-                                id='graph_1',
-                                figure=sexo_por_entidad(),
-                                style={'width': '50%'}
+                            
+                            dbc.RadioItems(
+                                id="radios",
+                                className="btn-group",
+                                inputClassName="btn-check",
+                                labelClassName="btn btn-outline-primary",
+                                labelCheckedClassName="active",
+                                options=[
+                                    {"label": "Género", "value": 1},
+                                    {"label": "Edad", "value": 2},
+                                    {"label": "Suma Aseg", "value": 3},
+                                ],
+                                value=1,
                             ),
-                            dcc.Graph(
-                                id='graph_3',
-                                figure=piramide_poblacional(),
-                                style={'width': '50%'}
-                            ),
-                            dcc.Graph(
-                                id='graph_4',
-                                figure=plot_barras(),
-                                style={'width': '50%'}
-                            ), 
+                        
+                            html.Div(id="output"),
                         ]  
                     ),
                     className='mt-3'                
@@ -53,21 +55,21 @@ app.layout =dbc.Container([
                         [
                             html.H2('Características de las pólizas de seguro'),
                             html.Hr(),
-                            dcc.Graph(
-                                id='graph_2',
-                                figure=formas_ventas(),
-                                style={'width': '50%'}
+                            
+                            dbc.RadioItems(
+                                id="radios-2",
+                                className="btn-group",
+                                inputClassName="btn-check",
+                                labelClassName="btn btn-outline-primary",
+                                labelCheckedClassName="active",
+                                options=[
+                                    {"label": "Forma de venta", "value": 1},
+                                    {"label": "Modalida de póliza", "value": 2},
+                                    {"label": "Cobertura", "value": 3},
+                                ],
+                                value=1,
                             ),
-                             dcc.Graph(
-                                id='graph_5',
-                                figure=modalidad_poliza(),
-                                style={'width': '50%'}
-                            ),
-                            dcc.Graph(
-                                id='graph_6',
-                                figure=cobertura(),
-                                style={'width': '50%'}
-                            ),
+                            html.Div(id="output-2"),
                         ]  
                     ),
                     className='mt-3'                
@@ -80,24 +82,65 @@ app.layout =dbc.Container([
                         [
                             html.H2('Para qué se aseguran las personas?'),
                             html.Hr(),
-                            dcc.Graph(
-                                id='graph_7',
-                                figure=siniestros(),
-                                style={'width': '50%'}
+                            
+                            dbc.RadioItems(
+                                id="radios-3",
+                                className="btn-group",
+                                inputClassName="btn-check",
+                                labelClassName="btn btn-outline-primary",
+                                labelCheckedClassName="active",
+                                options=[
+                                    {"label": "Siniestros 1", "value": 1},
+                                    {"label": "Siniestros 2", "value": 2},
+                                ],
+                                value=1,
                             ),
-                            dcc.Graph(
-                                id='graph_8',
-                                figure=siniestros_por_monto_pagado(),
-                                style={'width': '50%'}
-                            ),
+                            html.Div(id="output-3"),
                         ]  
                     ),
                     className='mt-3'                
                 ),
                 label='Para qué', active_tab_style={"textTransform": "uppercase"},
                 activeTabClassName="fw-bold"),        
-        ])
+            ])
 ])
+
+@app.callback(Output("output", "children"), [Input("radios", "value")])
+def display_value_1(value):
+    if value == 1:
+        # Crear la gráfica para la opción 1
+        fig = sexo_por_entidad()
+    elif value == 2:
+        # Crear la gráfica para la opción 2
+        fig = piramide_poblacional()
+    elif value == 3:
+        # Crear la gráfica para la opción 3
+        fig = plot_barras()
+    return dcc.Graph(figure=fig)
+
+@app.callback(Output("output-2", "children"), [Input("radios-2", "value")])
+def display_value_2(value):
+    if value == 1:
+        # Crear la gráfica para la opción 1
+        fig = formas_ventas()
+    elif value == 2:
+        # Crear la gráfica para la opción 2
+        fig = modalidad_poliza()
+    elif value == 3:
+        # Crear la gráfica para la opción 2
+        fig = cobertura()
+    return dcc.Graph(figure=fig)
+
+@app.callback(Output("output-3", "children"), [Input("radios-3", "value")])
+def display_value_3(value):
+    if value == 1:
+        # Crear la gráfica para la opción 1
+        fig = siniestros()
+    elif value == 2:
+        # Crear la gráfica para la opción 2
+        fig = siniestros_por_monto_pagado()
+    return dcc.Graph(figure=fig)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8052)
--- dash3.py
+++ dash3.py
@@ -5,38 +5,95 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 
 from graph import *
+from dash import Input, Output, html
 
-carousel=dbc.Carousel(
-    items=[
-        {"key": "1", "children": dcc.Graph(
-                                    id='graph_4',
-                                    figure=plot_barras(),
-                                    style={'width': '50%'}
-            )
-        },
-        {"key": "2", "children":dcc.Graph(
-                                    id='graph_3',
-                                    figure=piramide_poblacional(),
-                                    style={'width': '50%'}
-                                    )
-        },
-        {"key": "3", "children": dcc.Graph(
-                                    id='graph_1',
-                                    figure=sexo_por_entidad(),
-                                    style={'width': '50%'}
-                                    )
-        }
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div(
+    [
+        dbc.RadioItems(
+            id="radios-1",
+            className="btn-group",
+            inputClassName="btn-check",
+            labelClassName="btn btn-outline-primary",
+            labelCheckedClassName="active",
+            options=[
+                {"label": "Género", "value": 1},
+                {"label": "Edad", "value": 2},
+                {"label": "Suma Aseg", "value": 3},
+            ],
+            value=1,
+        ),
+         html.Div(id="output"),
+
+        dbc.RadioItems(
+            id="radios-2",
+            className="btn-group",
+            inputClassName="btn-check",
+            labelClassName="btn btn-outline-primary",
+            labelCheckedClassName="active",
+            options=[
+                {"label": "Forma de venta", "value": 4},
+                {"label": "Modalida de póliza", "value": 5},
+                {"label": "Cobertura", "value": 6},
+            ],
+            value=1,
+        ),
+         html.Div(id="output"),
+
+        dbc.RadioItems(
+            id="radios-3",
+            className="btn-group",
+            inputClassName="btn-check",
+            labelClassName="btn btn-outline-primary",
+            labelCheckedClassName="active",
+            options=[
+                {"label": "Siniestros 1", "value": 7},
+                {"label": "Siniestros 2", "value": 8},
+            ],
+            value=1,
+        ),
+        html.Div(id="output"),
     ],
-    controls=True,
-    indicators=True,
+    className="radio-group",
 )
 
+@app.callback(Output("output", "children"), [Input("radios-1", "value"), Input("radios-2", "value"), Input("radios-3", "value")])
+def display_value(value1, value2, value3):
+    if value1 == 1:
+        # Crear la gráfica para la opción 1
+        fig = sexo_por_entidad()
+    elif value1 == 2:
+        # Crear la gráfica para la opción 2
+        fig = piramide_poblacional()
+    elif value1 == 3:
+        # Crear la gráfica para la opción 3
+        fig = plot_barras()
+    elif value2 == 4:
+        # Crear la gráfica para la opción 4
+        fig = formas_ventas()
+    elif value2 == 5:
+        # Crear la gráfica para la opción 5
+        fig = modalidad_poliza()
+    elif value2 == 6:
+        # Crear la gráfica para la opción 6
+        fig = cobertura()
+    elif value3 == 7:
+        # Crear la gráfica para la opción 7
+        fig = siniestros()
+    elif value3 == 8:
+        # Crear la gráfica para la opción 8
+        fig = siniestros_por_monto_pagado()
+    else:
+        # Opción por defecto si el valor no coincide con ninguna opción
+        fig = go.Figure()
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+    # Devolver la gráfica como salida
+    return dcc.Graph(figure=fig)
 
-# Agregar el carrusel a la aplicación Dash
-app.layout = dbc.Container(carousel, className="mt-4")
 
 # Ejecutar la aplicación Dash
 if __name__ == '__main__':
     app.run_server(debug=True, port=8053)
+    
+    
